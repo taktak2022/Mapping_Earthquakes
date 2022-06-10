b5a2b75f25fa938907ef9f765c9260155609e0c3
Takemi <tsoshiro@gmail.com>
2022-06-09 19:30:38 -0700

    2nd commit for Mapping_Single_points.  Initial commit only copied files & folders but no code.  ???

---
+++ Mapping_Single_Points/static/js/logic.js
@@ -0,0 +1,20 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// MODULE 13.2.4 ADD A MAP OBJECT: 
+// SEE CODE OPTION TO *NOT* USE setView function
+// USED TO ADD MULTIPLE TILE LAYERS OR BACKGROUND IMAGES
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
