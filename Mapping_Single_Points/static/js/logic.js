5def228d79bfbeaa523899949a567c3853319d6f
Takemi <tsoshiro@gmail.com>
2022-06-09 23:27:42 -0700

    code changes on logic.js on Mapping_Single_Points branch; variations to map, marker types & colors.

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -7,11 +7,22 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+// MODULE 13.4.1 * ADD A MARKER TO THE MAP *
+// Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//MODULE 13.4.1 * ADD A CIRCLE RADIUS TO THE MAP *; * YELLOW CIRCLE ON DARK MAP *
+L.circleMarker([34.0522, -118.2437], {
+    radius: 20,
+    color: "black",
+    fillColor: "magenta"
+ }).addTo(map);
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
