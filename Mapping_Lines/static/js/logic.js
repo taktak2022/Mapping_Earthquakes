58e66b27c34337cacce2d77abb5a08137f252943
Takemi <tsoshiro@gmail.com>
2022-06-10 18:26:42 -0700

    1st commit to new branch Mapping Lines.

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,47 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// MODULE 13.2.4 ADD A MAP OBJECT: 
+// SEE CODE OPTION TO *NOT* USE setView function
+// USED TO ADD MULTIPLE TILE LAYERS OR BACKGROUND IMAGES
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// MODULE 13.4.1 * ADD A MARKER TO THE MAP *
+// Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//MODULE 13.4.1 * ADD A CIRCLE RADIUS TO THE MAP *; * YELLOW CIRCLE ON DARK MAP *
+//L.circleMarker([34.0522, -118.2437], {
+    //radius: 20,
+    //color: "black",
+    //fillColor: "magenta"
+ //}).addTo(map);
+
+// MODULE 13.4.2 ADD A NEW FILE FOR DATA AND ASSIGN VARIABLE
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: "black",
+        fillColor: "magenta"
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+   });
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
